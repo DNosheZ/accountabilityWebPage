6be28476bca0e7d028ff689de25ae4a4b244702e
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-06-04 19:34:57 -0500

    first draft

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function redirectTo(origen,fin) {
+    let div1 = document.getElementById(origen);
+    let div2 = document.getElementById(fin);
+
+    div1.style.display = 'none';
+    div2.style.display = 'block';
+}
+function register(){
+
+}
+function login(){}
